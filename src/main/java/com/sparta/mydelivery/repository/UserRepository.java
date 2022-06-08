6b139a5c1a7b773e395af6f83f75a72be299f88b
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-06-08 16:02:10 +0900

    쿼리 팁

--- src/main/java/com/sparta/mydelivery/repository/UserRepository.java
+++ src/main/java/com/sparta/mydelivery/repository/UserRepository.java
@@ -2,9 +2,18 @@ package com.sparta.mydelivery.repository;
 
 import com.sparta.mydelivery.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
+
+    @Query("select this from User this where this.username=:seyeal")
+    List<User> getUsers(@Param("seyeal") String name);
+
+
 }
